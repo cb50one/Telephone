5d1b43c60dd359709dfd20c747a587d840bece9d
Nakajima Takumu <z400@cb50one.local>
2020-02-03 13:05:32 +0900

    create Telephone(unity app)

---
+++ Assets/Script/Game.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    public Text timerText;
+    public AudioSource audioSource;
+    public float countdown;
+    int seconds;
+    bool gameStart = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float SoundTime = Random.Range(4f, 10f);
+        Invoke("Sound", SoundTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        seconds = (int)countdown;
+        timerText.text = seconds.ToString();
+
+        if (countdown < 0)
+        {
+            timerText.text = "";
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            if (gameStart)
+            {
+                SceneManager.LoadScene("FWin");
+            }
+            else
+            {
+                SceneManager.LoadScene("JWin");
+            }
+        }
+
+        if (Input.GetKey(KeyCode.J))
+        {
+            if (gameStart)
+            {
+                SceneManager.LoadScene("JWin");
+            }
+            else
+            {
+                SceneManager.LoadScene("FWin");
+            }
+        }
+    }
+
+    void Sound()
+    {
+        audioSource.Play();
+        gameStart = true;
+    }
+}
---
+++ Assets/Script/Preparation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Preparation : MonoBehaviour
+{
+    public Text LeftText;
+    public Text RightText;
+    bool preparationL = false;
+    bool preparationR = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            LeftText.text = "OK";
+            preparationL = true;
+            GameStart();
+        }
+
+        if (Input.GetKey(KeyCode.J))
+        {
+            RightText.text = "OK";
+            preparationR = true;
+            GameStart();
+        }
+    }
+
+    public void GameStart()
+    {
+        if (preparationL && preparationR)
+        {
+            SceneManager.LoadScene("game");
+        }
+    }
+}
